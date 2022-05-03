32ef661ae2255651e4812fa9e93e92e06cc3a44e
Vinicius Braga <Vbragadev@gmail.com>
2022-05-02 23:54:12 -0300

    learning getchar and prinf

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    /* code */
+    printf("Hello World");
+
+    printf("Pressione qualquer telcar para sair");
+
+    getchar();
+    return 0;
+}
