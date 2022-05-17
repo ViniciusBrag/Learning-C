e1def74ca659d5f714d3ac782417097c1ce23f03
Vinicius Braga <Vbragadev@gmail.com>
2022-05-16 23:27:33 -0300

    Add aprendizado sobre string

---
+++ string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* code */
+    char sexo;
+    
+    printf("Digite seu sexo (f, F, m ou M): ");
+    scanf("%c",&sexo);
+
+    printf("Valor da variavel sexo: %c", sexo);
+
+
+    return 0;
+}
+
