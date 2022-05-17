f432dd73743369e08624450340ccbd8a9cce01b7
Vinicius Braga <Vbragadev@gmail.com>
2022-05-16 23:33:07 -0300

    add aprenzidado sobre getchar

---
+++ getchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* code */
+    char letra;
+
+    printf("Digite uma letra: ");
+    letra = getchar();
+    printf("Caracter lido: %c", letra);
+    return 0;
+}
